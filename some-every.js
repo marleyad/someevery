66b7ca83c48c7c37c1af8f5e4901d3c7e1c9e56a
Adam Marley <141704661+marleyad@users.noreply.github.com>
2024-11-16 21:42:51 -0500

    Update some-every.js

--- some-every.js
+++ some-every.js
@@ -1,6 +1,6 @@
 /* 
 Write a function called hasOddNumber which accepts an array and returns true if the array contains at least one odd number, otherwise, it returns false.
-
+ 
 Examples:
     hasOddNumber([1,2,2,2,2,2,4]) // true
     hasOddNumber([2,2,2,2,2,4]) // false
